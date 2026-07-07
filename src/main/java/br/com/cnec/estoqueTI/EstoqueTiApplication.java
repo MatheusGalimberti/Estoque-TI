415994289c4c01d1fb3271da1ac11baf7a7fc3cb
Matheus Padilha Galimberti <matheus.padilhagalimberti@gmail.com>
2026-07-06 21:53:40 -0300

    chore: initialize Spring Boot project

---
+++ src/main/java/br/com/cnec/estoqueTI/EstoqueTiApplication.java
@@ -0,0 +1,16 @@
+package br.com.cnec.estoque_TI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueTiApplication {
+
+	public static void main(String[] args) {
+
+
+
+
+	}
+
+}
